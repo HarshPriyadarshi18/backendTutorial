f3e248ace881a14d6fb7d1ac2931869e9a35697a
harsh <harshp.ug23.cs@nitp.ac.in>
2025-07-17 16:05:58 +0530

    mongoose with mogodb CRUD operation

--- curd-opr-mongoose/index.js
+++ curd-opr-mongoose/index.js
@@ -20,7 +20,17 @@ app.post('/api/enquiry-insert',(req,res)=>{
     })
  
 })
-
+app.get('/api/enquiry-list',async(req,res)=>{
+    let enquiryList=await EnquiryModel.find();
+    res.send({status:1,message:"Enquiry list fetched successfully",data: enquiryList});
+   // res.status(200).json({status:1,message:"Enquiry list fetched successfully",data:[]});
+}
+)
+app.delete('/api/enquiry-delete/:id',async(req,res)=>{
+    let enquiryId=req.params.id;
+    let deleteEnquiry=await EnquiryModel.deleteOne({_id:enquiryId});
+    res.send({status:1,message:"Enquiry deleted successfully",data:deleteEnquiry});
+})
 //connect to MongoDB
 mongoose.connect(process.env.DBURL).then(()=>{
     console.log("Connected to MongoDB");
